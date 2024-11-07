2c8acb8b2a1ed3e7af222c572b9612a10aa5f786
Ethan Dean <ethandean7@gmail.com>
2024-11-07 16:47:27 -0500

    Update script

--- btc_prices.py
+++ btc_prices.py
@@ -1,7 +1,7 @@
 import time
 import requests
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Set up your API key and endpoint
 API_KEY = 'API_KEY'
@@ -18,14 +18,11 @@ HEADERS = {
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Base directory for logs
-BASE_LOG_PATH = os.path.join(BASE_DIR, 'bitcoin_logs')
+BASE_LOG_PATH = os.path.join(BASE_DIR, 'data_logs')
 os.makedirs(BASE_LOG_PATH, exist_ok=True)
 
 # Error log directory
 ERROR_LOG_PATH = os.path.join(BASE_DIR, 'error.log')
-
-# First day file to store the initial timestamp
-FIRST_DAY_FILE = os.path.join(BASE_LOG_PATH, 'first_day.txt')
 
 def log_error(message):
     # Append an error message to the error log
@@ -33,13 +30,11 @@ def log_error(message):
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         f.write(f"{timestamp} - ERROR - {message}\n")
 
-def get_first_block_timestamp():
-    # Return block time of bitcoin genesis block
-    return datetime.fromtimestamp(1231006505)
-
-def get_day_count_since_first_day(first_day_timestamp):
+def get_day_count_since_first_day():
     # Calculate the number of days since the first day
-    first_day = first_day_timestamp
+    # first day = day of block time from genesis block
+    # Bitcoin Genesis Block = January 3, 2009
+    first_day = datetime(2009, 1, 3)
     current_day = datetime.now()
     delta = current_day - first_day
     return delta.days
@@ -57,19 +52,14 @@ def fetch_bitcoin_price():
 
 def log_price_to_file(price, timestamp, day_count):
     # Log file path within the day-specific folder
-    log_file_path = os.path.join(BASE_LOG_PATH, str(day_count) + '.txt')
-    
+    log_file_path = os.path.join(BASE_LOG_PATH, 'day_' + str(day_count) + '.txt')
     # Append the data
     with open(log_file_path, 'a') as f:
         f.write(f"{timestamp} {price}\n")
 
 def main():
-    # Get the first day timestamp
-    first_day_timestamp = get_first_block_timestamp()
-    
     # Calculate the number of days since the first day
-    day_count = get_day_count_since_first_day(first_day_timestamp)
-    
+    day_count = get_day_count_since_first_day()
     # Fetch and log Bitcoin price
     price, timestamp = fetch_bitcoin_price()
     if price is not None and timestamp is not None:
